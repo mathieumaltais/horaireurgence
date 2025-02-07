de13b06325b595d6f9a643b1c82a5cf052adbd85
mathieumaltais <mathieu.maltais@live.ca>
2025-02-07 08:50:04 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,12 @@
-// 🔥 Firebase Configuration
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyBDwCt973qhUxk1_5UqTNDy82pef2GQFcc",
     authDomain: "horaireurgence-a488f.firebaseapp.com",
@@ -9,6 +17,10 @@
     measurementId: "G-Z67Y8T809L"
   };
 
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 // ✅ Initialize Firebase FIRST before using auth or db
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
